5df3b3b2e22e1eabcbfe207747dcb8f58a55e9ab
Amr Fateem <amr.fateem@yahoo.com>
2023-06-24 15:48:40 +0300

    Updating Styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import "./App.css";
 
 function App() {
   const [score, setScore] = useState(0);
@@ -31,14 +30,15 @@ function App() {
     playAgainButton.classList.remove("hidden");
     scoreDiv.classList.remove("hidden");
   };
+
   return (
-    <div className="container mx-auto p-4">
-      <div id="intro">
-        <h1 className="text-3xl font-bold text-center">Quizzical</h1>
-        <p>Some Text</p>
+    <div className=" mx-auto p-4 bg-gradient-to-r min-h-screen from-purple-500 to-blue-500 flex flex-col justify-center">
+      <div id="intro" className="text-center">
+        <h1 className="text-5xl font-bold text-white mb-4">Quizzical</h1>
+        <p className="text-white text-lg mb-8">Some Text</p>
         <button
           id="start"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 block mx-auto"
+          className="bg-pink-500 transition-all hover:bg-pink-700 text-white font-bold py-2 px-4 rounded mb-4 mx-auto w-1/2"
           onClick={() => {
             document.getElementById("intro").classList.add("hidden");
             document.getElementById("questions").classList.remove("hidden");
@@ -50,46 +50,35 @@ function App() {
       <div id="questions" className="hidden">
         {questionsAndAnswers.map((item, index) => {
           return (
-            <div className="flex flex-col justify-center gap-5" key={index}>
-              <h3 className="text-xl font-bold">{item.question}</h3>
-              <div className="flex gap-5 mt-2 items-center justify-center">
+            <div className="flex flex-col justify-center gap-5 bg-white bg-opacity-80 p-6 rounded-lg" key={index}>
+              <h3 className="text-2xl font-bold">{item.question}</h3>
+              <div className="flex flex-wrap gap-4 mt-4">
                 {item.answers.map((answer, answerIndex) => {
                   return (
-                    <div key={answerIndex}>
-                      <input
-                        type="radio"
-                        name={`answer-${index}`}
-                        className=""
-                        value={answer}
-                        checked={selectedAnswers[index] === answer}
-                        onChange={() => {
-                          const updatedSelectedAnswers = [...selectedAnswers];
-                          updatedSelectedAnswers[index] = answer;
-                          setSelectedAnswers(updatedSelectedAnswers);
-                        }}
-                      />
-                      <label
-                        className="ml-2 rounded-full cursor-pointer"
-                        onClick={() => {
-                          const updatedSelectedAnswers = [...selectedAnswers];
-                          updatedSelectedAnswers[index] = answer;
-                          setSelectedAnswers(updatedSelectedAnswers);
-                        }}
-                      >
-                        {answer}
-                      </label>
-                    </div>
+                    <button
+                      key={answerIndex}
+                      className={`bg-blue-500 transition-all hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 ${
+                        selectedAnswers[index] === answer ? "bg-blue-800" : ""
+                      }`}
+                      onClick={() => {
+                        const updatedSelectedAnswers = [...selectedAnswers];
+                        updatedSelectedAnswers[index] = answer;
+                        setSelectedAnswers(updatedSelectedAnswers);
+                      }}
+                    >
+                      {answer}
+                    </button>
                   );
                 })}
               </div>
-              <hr className="my-4" />
+              <hr className="my-4 border-gray-500" />
             </div>
           );
         })}
-        <div className="flex justify-center items-center mt-4">
+        <div className="flex justify-center items-center mt-8">
           <button
             id="check"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded hidden"
+            className="bg-blue-500 transition-all hover:bg-blue-700 text-white font-bold py-2 px-4 rounded hidden"
             onClick={() => {
               handleChange();
               setScore(0);
@@ -104,11 +93,13 @@ function App() {
           </button>
         </div>
       </div>
-      <div className="text-center mt-4 hidden" id="score">
-        You scored <span className="font-bold">{score}</span>/5 correct answers
+      <div className="text-center mt-8 hidden" id="score">
+        <p className="text-lg text-white">
+          You scored <span className="font-bold">{score}</span>/5 correct answers
+        </p>
         <button
           id="play-again"
-          className="bg-blue-500 hover:bg-blue-700 ml-3 text-white font-bold py-2 px-4 rounded hidden "
+          className="bg-blue-500 transition-all hover:bg-blue-700 ml-3 text-white font-bold py-2 px-4 rounded hidden"
           onClick={() => {
             window.location.reload();
           }}
